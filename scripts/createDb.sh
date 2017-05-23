bf2b4cf29ac4ecdf8c17dd307f4b42029699f521
Jordie Diepeveen <jordie.diepeveen@trimm.nl>
2017-05-23 08:34:29 +0200

    Update createDb script

--- scripts/createDb.sh
+++ scripts/createDb.sh
@@ -3,16 +3,14 @@ ROOT_PASS=$1
 NEW_USER=$2
 NEW_PASS=$3
 
+# Create application-specific user
+# And disable remote root login
 mysql -uroot -p${ROOT_PASS} << END
     CREATE DATABASE magnolia_author;
-    CREATE USER '${NEW_USER}'@'%' IDENTIFIED BY '${NEW_PASS}';
-    GRANT ALL ON magnolia_author.* TO '${NEW_USER}'@'%';
-    FLUSH PRIVILEGES;
-END
-
-mysql -uroot -p${ROOT_PASS} << END
     CREATE DATABASE magnolia_public;
-    CREATE USER '${NEW_USER}'@'%' IDENTIFIED BY '${NEW_PASS}';
-    GRANT ALL ON magnolia_public.* TO '${NEW_USER}'@'%';
+    CREATE USER '${NEW_USER}'@'localhost' IDENTIFIED BY '${NEW_PASS}';
+    GRANT ALL ON magnolia_author.* TO '${NEW_USER}'@'localhost';
+    GRANT ALL ON magnolia_public.* TO '${NEW_USER}'@'localhost';
+    DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
     FLUSH PRIVILEGES;
 END
