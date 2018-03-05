24a365e5b4e2e41bd051039094aa3cb73f2e2f36
Jordie Diepeveen <jordie.diepeveen@trimm.nl>
2018-03-05 08:19:09 +0100

    Added separate docker container for public Moved success text to separate file

--- magnolia/scripts/createDb.sh
+++ magnolia/scripts/createDb.sh
@@ -7,24 +7,24 @@ HOST=$4
 # Create application-specific user
 # And disable remote root login
 mysql -uroot -p${ROOT_PASS} << END
-    CREATE DATABASE IF NOT EXISTS author CHARACTER SET utf8 COLLATE utf8_general_ci;
-    CREATE DATABASE IF NOT EXISTS public CHARACTER SET utf8 COLLATE utf8_general_ci;
+    CREATE DATABASE IF NOT EXISTS magnolia-author CHARACTER SET utf8 COLLATE utf8_general_ci;
+    CREATE DATABASE IF NOT EXISTS magnolia-public CHARACTER SET utf8 COLLATE utf8_general_ci;
 
     CREATE USER '${NEW_USER}'@'localhost' IDENTIFIED BY '${NEW_PASS}';
-    GRANT ALL ON author.* TO '${NEW_USER}'@'localhost';
-    GRANT ALL ON public.* TO '${NEW_USER}'@'localhost';
+    GRANT ALL ON magnolia-author.* TO '${NEW_USER}'@'localhost';
+    GRANT ALL ON magnolia-public.* TO '${NEW_USER}'@'localhost';
 
     CREATE USER '${NEW_USER}'@'127.0.0.1' IDENTIFIED BY '${NEW_PASS}';
-    GRANT ALL ON author.* TO '${NEW_USER}'@'127.0.0.1';
-    GRANT ALL ON public.* TO '${NEW_USER}'@'127.0.0.1';
+    GRANT ALL ON magnolia-author.* TO '${NEW_USER}'@'127.0.0.1';
+    GRANT ALL ON magnolia-public.* TO '${NEW_USER}'@'127.0.0.1';
 
     CREATE USER '${NEW_USER}'@'::1' IDENTIFIED BY '${NEW_PASS}';
-    GRANT ALL ON author.* TO '${NEW_USER}'@'::1';
-    GRANT ALL ON public.* TO '${NEW_USER}'@'::1';
+    GRANT ALL ON magnolia-author.* TO '${NEW_USER}'@'::1';
+    GRANT ALL ON magnolia-public.* TO '${NEW_USER}'@'::1';
 
     CREATE USER '${NEW_USER}'@'${HOST}' IDENTIFIED BY '${NEW_PASS}';
-    GRANT ALL ON author.* TO '${NEW_USER}'@'${HOST}';
-    GRANT ALL ON public.* TO '${NEW_USER}'@'${HOST}';
+    GRANT ALL ON magnolia-author.* TO '${NEW_USER}'@'${HOST}';
+    GRANT ALL ON magnolia-public.* TO '${NEW_USER}'@'${HOST}';
 
     DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
     FLUSH PRIVILEGES;
---
+++ text/success.md
@@ -0,0 +1,19 @@
+### Magnolia Author Instance
+URL: ${env.url}  
+Catalina home: ${globals.CATALINA_HOME}  
+Data: ${globals.MGNL_REPO_PATH}/author  
+DB name: ${globals.DB_NAME_AUTHOR}  
+DB user: magnolia@localhost  
+
+### Magnolia Public Instance
+URL: ${env.url}magnoliaAuthor/  
+Catalina home: ${globals.CATALINA_HOME}  
+Data: ${globals.MGNL_REPO_PATH}/public  
+DB name: ${globals.DB_NAME_AUTHOR}  
+DB user: magnolia@localhost  
+
+### Maintenance Mode
+Maintenance page:
+
+
+[Image here displaying ]
