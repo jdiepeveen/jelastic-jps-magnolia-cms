0eecde240abb604e989f4b2ea6aae6305b0b3c87
Jordie Diepeveen <jordie.diepeveen@trimm.nl>
2018-03-29 11:41:36 +0200

    Updated createDB script

--- magnolia/scripts/createDb.sh
+++ magnolia/scripts/createDb.sh
@@ -7,10 +7,10 @@ NEW_PASS=$3
 # Create application-specific user
 # And disable remote root login
 mysql -u${USER} -p${PASSWORD} << END
-    CREATE DATABASE IF NOT EXISTS magnolia-public;
+    CREATE DATABASE IF NOT EXISTS magnolia_public;
 
     CREATE USER '${NEW_USER}'@'%' IDENTIFIED BY '${NEW_PASS}';
-    GRANT ALL ON magnolia-public.* TO '${NEW_USER}'@'%';
+    GRANT ALL ON magnolia_public.* TO '${NEW_USER}'@'%';
 
     DELETE FROM mysql.user WHERE user='root' AND host NOT IN ('localhost', '127.0.0.1', '::1');
     FLUSH PRIVILEGES;
