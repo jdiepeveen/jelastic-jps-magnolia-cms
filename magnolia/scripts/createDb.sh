746795a357d8c4a6632113349668c190cd2647f6
Jordie Diepeveen <jordie.diepeveen@trimm.nl>
2018-03-29 10:50:14 +0200

    Updated createDB script

--- magnolia/scripts/createDb.sh
+++ magnolia/scripts/createDb.sh
@@ -1,31 +1,27 @@
 #!/bin/bash
-ROOT_PASS=$1
+USER="root"
+PASSWORD=$1
 NEW_USER=$2
 NEW_PASS=$3
 HOST=$4
 
 # Create application-specific user
 # And disable remote root login
-mysql -uroot -p${ROOT_PASS} << END
-    CREATE DATABASE IF NOT EXISTS magnolia-author CHARACTER SET utf8 COLLATE utf8_general_ci;
+mysql -u${USER} -p${PASSWORD} << END
     CREATE DATABASE IF NOT EXISTS magnolia-public CHARACTER SET utf8 COLLATE utf8_general_ci;
 
     CREATE USER '${NEW_USER}'@'localhost' IDENTIFIED BY '${NEW_PASS}';
-    GRANT ALL ON magnolia-author.* TO '${NEW_USER}'@'localhost';
     GRANT ALL ON magnolia-public.* TO '${NEW_USER}'@'localhost';
 
     CREATE USER '${NEW_USER}'@'127.0.0.1' IDENTIFIED BY '${NEW_PASS}';
-    GRANT ALL ON magnolia-author.* TO '${NEW_USER}'@'127.0.0.1';
     GRANT ALL ON magnolia-public.* TO '${NEW_USER}'@'127.0.0.1';
 
     CREATE USER '${NEW_USER}'@'::1' IDENTIFIED BY '${NEW_PASS}';
-    GRANT ALL ON magnolia-author.* TO '${NEW_USER}'@'::1';
     GRANT ALL ON magnolia-public.* TO '${NEW_USER}'@'::1';
 
     CREATE USER '${NEW_USER}'@'${HOST}' IDENTIFIED BY '${NEW_PASS}';
-    GRANT ALL ON magnolia-author.* TO '${NEW_USER}'@'${HOST}';
     GRANT ALL ON magnolia-public.* TO '${NEW_USER}'@'${HOST}';
 
-    DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+    DELETE FROM mysql.user WHERE user='root' AND host NOT IN ('localhost', '127.0.0.1', '::1');
     FLUSH PRIVILEGES;
 END
