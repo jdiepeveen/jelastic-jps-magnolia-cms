390cf4d92972e83c452b595aae471e073865fb22
Jordie Diepeveen <jordie.diepeveen@trimm.nl>
2018-04-05 17:04:51 +0200

    Added installing rsync after installation for tomcat because it's based on alpine linux Updated createDb script

--- magnolia/scripts/createDb.sh
+++ magnolia/scripts/createDb.sh
@@ -7,11 +7,17 @@ NEW_PASS=$3
 # Create application-specific user
 # And disable remote root login
 mysql -u${USER} -p${PASSWORD} << END
+    -- Create magnolia_public database
     CREATE DATABASE IF NOT EXISTS magnolia_public;
 
-    CREATE USER '${NEW_USER}'@'%' IDENTIFIED BY '${NEW_PASS}';
+    -- Create 'root'@'127.0.0.1' which was removed by install script of Percona
+    CREATE USER 'root'@'127.0.0.1' IDENTIFIED BY '${PASSWORD}';
+    GRANT ALL ON *.* TO 'root'@'127.0.0.1' WITH GRANT OPTION;
+
+    -- Grant access for ${NEW_USER} on the database
     GRANT ALL ON magnolia_public.* TO '${NEW_USER}'@'%';
 
-    DELETE FROM mysql.user WHERE user='root' AND host NOT IN ('localhost', '127.0.0.1', '::1');
+    -- Remove remote access for root
+    DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
     FLUSH PRIVILEGES;
 END
